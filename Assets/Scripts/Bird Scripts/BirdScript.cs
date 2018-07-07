36d37175ac7f63807aca75cccddb1c3db3376f56
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-07 11:11:30 +0600

    Flappy bird

--- Assets/Scripts/Bird Scripts/BirdScript.cs
+++ Assets/Scripts/Bird Scripts/BirdScript.cs
@@ -75,8 +75,8 @@ public class BirdScript : MonoBehaviour {
 		else
 		{
 			float angle = 0;
-			//angle = Mathf.Lerp(0, -45, -myRigidBody.velocity.y / 7);
-			angle = Mathf.Lerp(0, -55, -myRigidBody.velocity.y / 7);
+			angle = Mathf.Lerp(0, -45, -myRigidBody.velocity.y / 7);
+			//angle = Mathf.Lerp(0, -55, -myRigidBody.velocity.y / 7);
 			//angle = Mathf.Lerp(0, -90, -myRigidBody.velocity.y / 7);
 			transform.rotation = Quaternion.Euler(0,0,angle);
 		}
