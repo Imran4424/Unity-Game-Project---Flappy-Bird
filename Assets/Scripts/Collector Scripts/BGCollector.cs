ca385e93d93b6eae79e68b246e8fd243930243b0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-07 12:18:51 +0600

    Flappy bird

--- Assets/Scripts/Collector Scripts/BGCollector.cs
+++ Assets/Scripts/Collector Scripts/BGCollector.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BGCollector : MonoBehaviour {
 
+	private GameObject[] backgrounds;
+	private GameObject[] grounds;
+
 	// Use this for initialization
 	void Start () {
 		
