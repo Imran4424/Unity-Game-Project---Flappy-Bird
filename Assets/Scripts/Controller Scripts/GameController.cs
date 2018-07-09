9405af98cbad4103b06241959de27d6ca76676e4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-09 19:15:33 +0600

    flappy Bird

--- Assets/Scripts/Controller Scripts/GameController.cs
+++ Assets/Scripts/Controller Scripts/GameController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	
+	public static GameController instance;
+
+	public const string High_Score = "High Score";
+	public const string Selected_Bird = "Selected Bird";
+	public const string Green_Bird = "Green Bird";
+	public const string Red_Bird = "Red Bird";
 
 
 	// Use this for initialization
