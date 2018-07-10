1f5ab26f19760384bffb29b4dbc86743323fcc08
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-10 23:22:28 +0600

    flappy Bird

--- Assets/Scripts/Controller Scripts/GamePlayController.cs
+++ Assets/Scripts/Controller Scripts/GamePlayController.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlayController : MonoBehaviour {
+
+	public static GamePlayController instance;
+
+	[SerializeField]
+	private Text
 
 	// Use this for initialization
 	void Start () {
