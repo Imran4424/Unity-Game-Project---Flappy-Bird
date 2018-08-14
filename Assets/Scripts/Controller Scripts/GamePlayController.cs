31c57a0dcb5bb60d0f156c70dc37093c0d9cfa24
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-14 07:19:02 +0600

    flappy

--- Assets/Scripts/Controller Scripts/GamePlayController.cs
+++ Assets/Scripts/Controller Scripts/GamePlayController.cs
@@ -351,6 +351,50 @@ public class GamePlayController : MonoBehaviour
 			{
 				GameController.instance.UnlockSpringLevel ();
 			}
+
+			if (GameController.instance.IsFarmLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockFarmLevel ();
+			}
+		}
+		else if (score >= 500 && score < 600)
+		{
+			medalImage.sprite = medals[2];
+
+			if (GameController.instance.IsGreenBirdUnlocked () == 0)
+			{
+				GameController.instance.UnlockGreenBird ();
+			}
+
+			if (GameController.instance.IsRedBirdUnlocked () == 0)
+			{
+				GameController.instance.UnlockRedBird ();
+			}
+
+			if (GameController.instance.IsBirdsLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockBirdsLevel ();
+			}
+
+			if (GameController.instance.IsMountainsLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockMountainsLevel ();
+			}
+
+			if (GameController.instance.IsLakeLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockLakeLevel ();
+			}
+
+			if (GameController.instance.IsSpringLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockSpringLevel ();
+			}
+
+			if (GameController.instance.IsFarmLevelUnlocked () == 0)
+			{
+				GameController.instance.UnlockFarmLevel ();
+			}
 		}
 	}
 }
