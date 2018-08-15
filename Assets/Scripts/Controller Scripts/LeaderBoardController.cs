34f2f999cf4aa01c8004a0a17bebc4716b9c3704
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-15 17:16:13 +0600

    flappy

---
+++ Assets/Scripts/Controller Scripts/LeaderBoardController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderBoardController : MonoBehaviour
+{
+
+	public static LeaderBoardController instance;
+
+	public const string leaderBoardId = "CgkIlY-bvcoNEAIQAQ";
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
