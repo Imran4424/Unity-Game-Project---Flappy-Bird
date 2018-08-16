234f2ae0146e4820272570503129d9b44742d774
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-16 10:33:51 +0600

    flappy bird

--- Assets/Scripts/Controller Scripts/BannerAdController.cs
+++ Assets/Scripts/Controller Scripts/BannerAdController.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using GoogleMobileAds;
-using GoogleMobileAds.Api;
 using GoogleMobileAds.Android;
+using GoogleMobileAds.Api;
 using UnityEngine;
 
-public class BannerAdController : MonoBehaviour 
+public class BannerAdController : MonoBehaviour
 {
 	public static BannerAdController instance;
 
 	private BannerView bannerView;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		
+		string appId = "ca-app-pub-8350504222422488~7708154151";
+
+		MobileAds.Initialize (appId);
+
+		string adUnitId = "ca-app-pub-8350504222422488/6121177109";
+
+		//this.showBannerAd (adUnitId);
 	}
-	
+
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		
+
 	}
 }
