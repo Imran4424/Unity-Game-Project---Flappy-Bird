0cb490eb6b4c35a86c270f4e0f68c0ed489c37ad
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-10-02 20:10:56 +0600

    Flappy

--- Assets/Scripts/Controller Scripts/MenuController.cs
+++ Assets/Scripts/Controller Scripts/MenuController.cs
@@ -61,7 +61,7 @@ public class MenuController : MonoBehaviour
 
 	IEnumerator WaitToLogin ()
 	{
-		yield return StartCoroutine (MyCoroutine.WaitForRealSeconds (2));
+		yield return StartCoroutine (MyCoroutine.WaitForRealSeconds (2f));
 
 		LeaderBoardController.instance.ConnectGooglePlayGames ();
 		AdsController.instance.CallAds();
